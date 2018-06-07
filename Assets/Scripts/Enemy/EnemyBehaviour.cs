f09b8fea106aa485b7242dd550e35f068234034b
Zeeshan Mohammed <zee_shan265@hotmail.com>
2018-06-07 08:30:07 -0400

    enemy spawns move towards tower and avoid placed objects..now to randomize and put a timer on them

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour {
+	public EnemyType enemy;
+	
+	public string enemyName;
+	public float currentHP, maxHP;
+	public float attackSpeed;
+	public float damage;
+	
+	public NavMeshAgent agent;
+	public Transform towerLocation;
+
+	void Awake() {
+		enemyName = enemy.name;
+		currentHP = enemy.currentHP;
+		maxHP = enemy.maxHP;
+		agent.speed = enemy.walkSpeed;
+		attackSpeed = enemy.attackSpeed;
+		damage = enemy.damage;
+
+		agent.SetDestination(towerLocation.position);
+	}
+}
